71e3b410e71f78c9c3da169aeee4020c18a9e015
unknown <Administrador@mga-ntian.benner.com.br>
2022-09-23 17:18:02 -0300

    adicionando script de criacao de tabelas

---
+++ SQL/CriacaoDasTabelas.sql
@@ -0,0 +1,53 @@
+Create table unidade
+
+(
+    id_unidade serial primary key,
+    nome varchar(200),
+   	endereco varchar(100),
+	localizacao varchar(50),
+	ativa bool
+)
+
+Create table vacina
+
+(
+    id_vacina serial primary key,
+    nome varchar(200)
+)
+
+Create table funcionario
+(
+    id_funcionario serial primary key,
+	id_unidade int not null,
+    nome varchar(200),
+   	login varchar(100),
+	senha varchar(50),
+	especialidade varchar(50),
+	FOREIGN KEY (id_unidade) REFERENCES unidade (id_unidade)
+)
+
+Create table servicos
+(
+    id_servicos serial primary key,
+    nome varchar(50)
+)
+
+Create table unidade_vacina
+(
+    id_unidade int not null,
+	id_vacina int not null,
+    disponivel bool,
+	FOREIGN KEY (id_unidade) REFERENCES unidade (id_unidade),
+	FOREIGN KEY (id_vacina) REFERENCES vacina (id_vacina)
+)
+
+Create table unidade_servico
+
+(
+    id_unidade int not null,
+	id_servicos int not null,
+    disponivel bool,
+	FOREIGN KEY (id_unidade) REFERENCES unidade (id_unidade),
+	FOREIGN KEY (id_servicos) REFERENCES servicos (id_servicos)
+)
+
